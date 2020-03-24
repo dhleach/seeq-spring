e3820cf402d40b690ff28c5ff5579b38f197c40c
Derek Leach <dhleach@gmail.com>
2020-03-24 10:42:11 -0400

    javascript communication working

--- seeq/src/main/webapp/js/myLocation.js
+++ seeq/src/main/webapp/js/myLocation.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
 	const locationButton = document.querySelector(".location-button");
 	locationButton.addEventListener("click", (event) => {
 		locationButton.setAttribute("color", "red");
+		locationButton.innerText = "Clicked!";
 	});
 	
 });
